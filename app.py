fbcf0bc09f35fcc5418b5317f58c3a54b7f9747d
Richard Pereira <109115443+Richard-Luis@users.noreply.github.com>
2026-08-29 12:19:39 -0300

    pronto

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 juros_fixo = 0.02
 
